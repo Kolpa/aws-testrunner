1bc45c2eddcdcf825e6f6ff5cc84474812aedbf4
Kolya Opahle <kolyaopahle@gmail.com>
2017-10-20 07:50:51 +0200

    make webdriver available in the test task module

--- task/TestTask.py
+++ task/TestTask.py
@@ -1,11 +1,18 @@
 import types
 import unittest
 import io
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 class TestTask:
     def __init__(self, teststr, testid):
         self.module = types.ModuleType('test')
+
+        chopts = Options()
+        chopts.add_argument('--headless')
+        self.module.driver = webdriver.Remote('http://localhost:5915', desired_capabilities=chopts.to_capabilities())
+        
         exec(teststr, self.module.__dict__)
 
         self.test_suite = unittest.TestLoader().loadTestsFromModule(self.module)
