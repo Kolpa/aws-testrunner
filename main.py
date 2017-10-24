f890f9761471b305d200c0b696b3019eef742307
Unknown <kolyaopahle@gmail.com>
2017-10-24 17:36:22 +0200

    self shutdown after 10 tries without any task

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from task import QueManager
 from time import sleep
+import os
 
+checks = 0
 queue = QueManager()
 
-while True:
+while checks < 10:
     print("Checking Tasks")
     task = queue.get_task()
     if task:
+        checks = 0
         result = task.run()
         queue.send_result(result.getvalue(), task.testid)
+    else:
+        checks += 1
+
     sleep(5)
+
+print("no new tasks for 50 seconds terminating instance...")
+os.system("poweroff")
